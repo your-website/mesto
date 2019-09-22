0780c26c63595709d3ed6cac3ecaa253da714677
Roman Kolpachev <roman.kolpachev@yandex.ru>
2019-09-22 22:18:01 +0700

    victory over webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // добавили плагин
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+// подключаем плагин
+const isDev = process.env.NODE_ENV === 'development';
+// создаем переменную для development-сборки
+
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
@@ -18,14 +23,40 @@ module.exports = {
                 }
             },
             {
-                test: /\.css$/,
-                use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] // добавили минификацию CSS
+                test: /\.css$/i,
+                use: [
+                    (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+                    'css-loader', 
+                    'postcss-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|ico|svg)$/,
+                use: [
+                        'file-loader?name=../images/[name].[ext]', // указали папку, куда складывать изображения
+                        {
+                            loader: 'image-webpack-loader',
+                            options: { }
+                        },
+                ],
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                loader: 'file-loader?name=./vendor/[name].[ext]'
             }
         ]
     },
     plugins: [
         new MiniCssExtractPlugin({ // 
             filename: 'style.[contenthash].css',
+        }),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
+                    preset: ['default'],
+            },
+            canPrint: true
         }),
         new HtmlWebpackPlugin({
             inject: false,
