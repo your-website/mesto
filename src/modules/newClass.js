1d0100b3f599a87f58e246e6e65ddf8a1d73c757
Roman Kolpachev <roman.kolpachev@yandex.ru>
2019-09-26 22:05:01 +0700

    fixed newClass

--- src/modules/newClass.js
+++ src/modules/newClass.js
@@ -1,9 +1,9 @@
 import {Popup} from './Popup.js';
 import {Api} from './Api.js';
-import {BASE_URL, AUTH_KEY} from './CONST_URL_KEY.js';
+import {AUTH_KEY} from './CONST_URL_KEY.js';
 import {CardList} from './CardList.js';
 export {popupy, api, cardlist};
-const serverUrl  = NODE_ENV === 'development' ? 'http://95.216.175.5/cohort2/' : 'http://95.216.175.5/cohort2/';
+const serverUrl  = NODE_ENV === 'development' ? 'https://praktikum.tk/cohort2/' : 'https://praktikum.tk/cohort2/';
 
 const initialCards = [];
 
