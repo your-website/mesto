dde6022b9f4d16e1a0ddecabbdf5bc4fa45d1ffc
Roman Kolpachev <roman.kolpachev@yandex.ru>
2019-09-26 21:43:31 +0700

    change url

--- src/modules/newClass.js
+++ src/modules/newClass.js
@@ -3,7 +3,7 @@ import {Api} from './Api.js';
 import {BASE_URL, AUTH_KEY} from './CONST_URL_KEY.js';
 import {CardList} from './CardList.js';
 export {popupy, api, cardlist};
-const serverUrl  = NODE_ENV === 'development' ? 'http://praktikum.tk/cohort2/' : 'http://praktikum.tk/cohort2/';
+const serverUrl  = NODE_ENV === 'development' ? 'http://95.216.175.5/cohort2/' : 'http://95.216.175.5/cohort2/';
 
 const initialCards = [];
 
