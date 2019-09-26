d5088210d1d04b1e665e62909292ca060475aed6
Roman Kolpachev <roman.kolpachev@yandex.ru>
2019-09-26 23:41:19 +0700

    omg its work

--- src/modules/newClass.js
+++ src/modules/newClass.js
@@ -3,10 +3,9 @@ import {Api} from './Api.js';
 import {BASE_URL, AUTH_KEY} from './CONST_URL_KEY.js';
 import {CardList} from './CardList.js';
 export {popupy, api, cardlist};
-const serverUrl  = NODE_ENV === 'development' ? 'http://praktikum.tk/cohort2/' : 'http://praktikum.tk/cohort2/';
+const serverUrl  = NODE_ENV === 'development' ? 'http://praktikum.tk/cohort2/' : 'https://praktikum.tk/cohort2/';
 
 const initialCards = [];
-
 const popupy = new Popup(document.querySelector('.root'));
 const api = new Api(serverUrl, AUTH_KEY);
 const cardlist = new CardList(document.querySelector('.places-list'), initialCards);
