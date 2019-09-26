3f9615d0c8803147ea95787ef1403fccb4869192
Roman Kolpachev <roman.kolpachev@yandex.ru>
2019-09-27 00:48:26 +0700

    del for pull

--- src/modules/CONST_URL_KEY.js
+++
@@ -1,2 +0,0 @@
-// export const BASE_URL = 'http://praktikum.tk/cohort2/';
-export const AUTH_KEY = 'd5a43806-1a31-432d-a441-75ca00a6201b';
--- src/modules/buttonNoneActive.js
+++
@@ -1,5 +0,0 @@
-export default function buttonNonActive(final) {
-    final.setAttribute('disabled', true);
-    final.style.color = "rgba(0, 0, 0, .2)";
-    final.style.backgroundColor = "#fff"
-}
